4e04dfdf83f147d982a83229e5d385dbf8df7a99
sychoi <cinnamon1995@naver.com>
2024-10-21 18:13:08 +0900

    3d 큐브를 화면에 생성 , 중요한 점은 카메라를 위치를 설정애줘야 오브젝트를 볼 수 있다.

--- 1-cube/src/main.js
+++ 1-cube/src/main.js
@@ -15,5 +15,18 @@ function init() {
   const scene = new THREE.Scene();
 
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500);
+
+  //박스모양의 지오메트리 인자 순서대로 각각 높이 너비 깊이
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+  const material = new THREE.MeshBasicMaterial({ color: 0xcc99ff });
+
+  const cube = new THREE.Mesh(geometry, material);
+
+  scene.add(cube);
+
+  // camera.position.z = 5;
+  camera.position.set(1, 2, 5);
+
   renderer.render(scene, camera);
 }
