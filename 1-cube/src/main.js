04edba93a71b3730d2fa3dd804e0f807114953eb
sychoi <cinnamon1995@naver.com>
2024-10-23 00:35:21 +0900

    다면체로 변경 및 두개의 오브젝트 추가

--- 1-cube/src/main.js
+++ 1-cube/src/main.js
@@ -18,54 +18,48 @@ function init() {
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500);
 
   //박스모양의 지오메트리 인자 순서대로 각각 높이 너비 깊이
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const cubeGeometry = new THREE.IcosahedronGeometry(1);
 
   /**
    *  MeshBasicMaterial은 조명에 영향을 받지 않는다. 조명이 없어도 가시적으로 확인이 가능
    *  MeshStandardMaterial은 조명에 영향을 받는다.
    */
-  const material = new THREE.MeshStandardMaterial({
-    color: new THREE.Color(0xcc99ff),
-    // transparent: true,
-    // opacity: 0.5,
-    // visible: false,
-    // wireframe: true,
-    //side : THREE.FrontSide, THREE.BackSide, THREE.DoubleSide
-    // side: THREE.DoubleSide,
+  const cubeMaterial = new THREE.MeshLambertMaterial({
+    color: 0x00ffff,
+    emissive: 0x11111,
   });
 
-  const cube = new THREE.Mesh(geometry, material);
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+  const skeletonGeometry = new THREE.IcosahedronGeometry(2);
+  const skeletonMaterial = new THREE.MeshBasicMaterial({
+    wireframe: true,
+    transparent: true,
+    opacity: 0.2,
+    color: 0xaaaaaa,
+  });
+  const skeleton = new THREE.Mesh(skeletonGeometry, skeletonMaterial);
 
   scene.add(cube);
+  scene.add(skeleton);
 
-  // camera.position.z = 5;
-  camera.position.set(1, 2, 2);
-
-  //lookAt을 넣어주면 항상 해당 오브젝트가 중심으로 보이게 된다.
-  camera.lookAt(cube.position);
+  camera.position.z = 5;
 
   //인자 순서 조명의 색, 강도 조절
-  const directionalLight = new THREE.DirectionalLight(0xf0f0f0, 1);
-  directionalLight.position.set(-1, 2, 3);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+
   scene.add(directionalLight);
-
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
-  scene.add(ambientLight);
-
   const clock = new THREE.Clock();
   render();
   //재귀적으로 함수를 실행시켜 돌아가는 애니메이션 만든다.
   function render() {
-    //각도의 기준은 radian
-    // cube.rotation.x = THREE.MathUtils.degToRad(45);
-    // cube.rotation.x += 0.01;
+    const elapsedTime = clock.getElapsedTime();
 
-    //Date.now 만 넣어줄 시 ms 단위로 값이 변화해서 겁나 도는 큐브가 나온다.
-    // cube.rotation.x = Date.now() / 1000;
+    cube.rotation.x = elapsedTime;
+    cube.rotation.y = elapsedTime;
 
-    //THREE.js 내장 함수
-    // cube.rotation.x = clock.getElapsedTime();
-    cube.rotation.x += clock.getDelta();
+    skeleton.rotation.x = elapsedTime * 1.5;
+    skeleton.rotation.y = elapsedTime * 1.5;
 
     // cube.rotation.y = Math.sin(cube.rotation.x);
     // cube.scale.x = Math.cos(cube.rotation.x);
