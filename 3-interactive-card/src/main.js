0df5011e56f0e2c94b8c438c6d8ffa8ab821a8cb
sychoi <cinnamon1995@naver.com>
2024-12-13 15:35:41 +0900

    배경을 넣는 여러가지 방법

--- 3-interactive-card/src/main.js
+++ 3-interactive-card/src/main.js
@@ -10,12 +10,18 @@ function init() {
     alpha: true
   });
 
-  renderer.setClearAlpha(0)
+  // renderer.setClearAlpha()  0~1까지 투명도를 조정할 수 있다.
+  //setClearColor 배경색과 투명도를 조절할 수 있다.
+  renderer.setClearColor(0x00aaff,0.5)
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   document.body.appendChild(renderer.domElement);
 
   const scene = new THREE.Scene();
+  const textureLoader = new THREE.TextureLoader()
+  const texture = textureLoader.load('https://plus.unsplash.com/premium_photo-1725408062872-c3692746be35?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c29mdCUyMHNreXxlbnwwfHwwfHx8MA%3D%3D')
+  scene.background = texture;
+  // scene.background = new THREE.Color(0xffaa00);
 
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500);
 
