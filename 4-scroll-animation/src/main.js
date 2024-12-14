171292d0f12b35053adb5598f5e33679f2bcadc3
sychoi <cinnamon1995@naver.com>
2024-12-14 13:36:32 +0900

    파도가 보여질 바닥 생성

--- 4-scroll-animation/src/main.js
+++ 4-scroll-animation/src/main.js
@@ -20,7 +20,23 @@ function init() {
 
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500);
 
-  camera.position.z = 5;
+  camera.position.set(0,25,150);
+
+  const waveGeometry = new THREE.PlaneGeometry(1500, 1500, 150, 150);
+  const waveMaterial = new THREE.MeshStandardMaterial({
+    color: '#00ffff',
+    wireframe: true
+  });
+
+  const wave = new THREE.Mesh(waveGeometry,  waveMaterial);
+  wave.rotation.x = -Math.PI / 2;
+
+  scene.add(wave);
+
+  const pointLight = new THREE.PointLight(0xffffff, 800);
+  pointLight.position.set(15,15,15);
+
+  scene.add(pointLight);
 
   render();
 
