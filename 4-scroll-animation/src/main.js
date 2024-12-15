2cf57321013fc8ea47272cf39e8ef2c876b5b78e
sychoi <cinnamon1995@naver.com>
2024-12-15 09:28:32 +0900

    모든 애니메이션 효과 부여 완료

--- 4-scroll-animation/src/main.js
+++ 4-scroll-animation/src/main.js
@@ -184,27 +184,63 @@ async  function init() {
     scrollTrigger: {
       trigger: '.wrapper',
       start:'top top',
-      markers: true,
+      end: 'bottom bottom',
+      // markers: true,
       scrub: true,
     }
   });
+
   tl
     .to(params, {
       waveColor:'#4268ff',
       onUpdate: () => {
         waveMaterial.color = new THREE.Color(params.waveColor);
-      }
+      },
+      duration: 1.5
     })
     .to(params, {
       backgroundColor: '#333b48',
       onUpdate: () => {
         scene.background = new THREE.Color(params.backgroundColor);
-      }
+      },
+      duration: 1.5
     }, '<')
     .to(params, {
       fogColors: '#2f2f2f',
       onUpdate: () => {
         scene.fog.color = new THREE.Color(params.fogColor);
-      }
+      },
+      duration: 1.5
     }, '<')
+    .to(camera.position, {
+      x:100,
+      z:-50,
+      duration: 2.5
+    })
+    .to(ship.position, {
+      z:150,
+      duration:2,
+    })
+    .to(camera.position,{
+      x:-50,
+      y:25,
+      z:100,
+      duration: 2,
+    })
+    .to(camera.position,{
+      x:0,
+      y:50,
+      z:300,
+      duration: 2,
+    });
+
+  gsap.to('.title', {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.wrapper',
+      scrub: true,
+      pin:true,
+      end: '+=1000',
+    },
+    });
 }
