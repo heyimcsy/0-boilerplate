fef4d4c27758cb0eaedd4cb0b0ed74acc06a996a
sychoi <cinnamon1995@naver.com>
2024-12-15 21:49:16 +0900

    새로운 particle 생성

---
+++ 5-fireworks/src/Firework.js
@@ -0,0 +1,34 @@
+import * as THREE from 'three';
+
+class Firework {
+  constructor ({x,y}) {
+    const count = 1000;
+
+    const particlesGeometry = new THREE.BufferGeometry();
+
+    const particles = [];
+
+    for (let i = 0; i < count; i++){
+      const particle = new THREE.Vector3(x, y, 0);
+
+      particles.push(particle);
+    }
+
+    particlesGeometry.setFromPoints(particles);
+
+    const textureLoader = new THREE.TextureLoader();
+    const texture = textureLoader.load('./assets/textures/particle.png');
+
+    const particlesMaterial = new THREE.PointsMaterial({
+      size: 1,
+      alphaMap:texture,
+      transparent:true,
+      depthWrite: false,
+    });
+    const points = new THREE.Points(particlesGeometry, particlesMaterial);
+
+    this.points = points;
+  }
+}
+
+export default Firework;
--- 5-fireworks/src/main.js
+++ 5-fireworks/src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import Firework from './Firework.js';
 
 window.addEventListener('load', function () {
   init();
@@ -22,50 +23,56 @@ function init() {
 
   new OrbitControls(camera, renderer.domElement);
 
-  const geometry = new THREE.BufferGeometry();
+  // const geometry = new THREE.BufferGeometry();
+  //
+  // const count = 10000;
+  //
+  // const positions = new Float32Array(count * 3);
+  // const colors = new Float32Array(count * 3);
+  //
+  // for (let i = 0 ; i < count ; i++){
+  //   // positions[i * 3] = Math.random() - 0.5;
+  //   // positions[i * 3 + 1] = Math.random() - 0.5;
+  //   // positions[i * 3 + 2] = Math.random() - 0.5;
+  //   //해당 Math.random() 함수처럼 사용할 수 있는 내장함수 randomFloatSpread 함수
+  //   positions[i * 3] = THREE.MathUtils.randFloatSpread(10);
+  //   positions[i * 3 + 1] = THREE.MathUtils.randFloatSpread(10);
+  //   positions[i * 3 + 2] = THREE.MathUtils.randFloatSpread(10);
+  //
+  //   colors[i * 3] = Math.random();
+  //   colors[i * 3 + 1] = Math.random();
+  //   colors[i * 3 + 2] = Math.random();
+  // }
+  //
+  // // 3 부분은 3개의 정보가 모여서 점하나의 정보를 담고있다고 알려주는 것
+  // geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+  // geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+  //
+  // const material = new THREE.PointsMaterial({
+  //   color: 0xccaaff,
+  //   size: 0.1,
+  //   vertexColors: true,
+  //   // sizeAttenuation: false,
+  // });
+  //
+  // const textureLoader = new THREE.TextureLoader();
+  //
+  // const texture = textureLoader.load('./assets/textures/particle.png');
+  //
+  // //넣어준 particle의 이미지가 배경까지 부자연스럽게 보여줘 해결해주는 속성값들
+  // material.alphaMap = texture;
+  // material.transparent = true;
+  // material.depthWrite = false;
+  //
+  // const points = new THREE.Points(geometry, material);
+  //
+  // scene.add(points);
 
-  const count = 10000;
+  const firework = new Firework({x:0 , Y: 0});
 
-  const positions = new Float32Array(count * 3);
-  const colors = new Float32Array(count * 3);
+  scene.add(firework.points);
 
-  for (let i = 0 ; i < count ; i++){
-    // positions[i * 3] = Math.random() - 0.5;
-    // positions[i * 3 + 1] = Math.random() - 0.5;
-    // positions[i * 3 + 2] = Math.random() - 0.5;
-    //해당 Math.random() 함수처럼 사용할 수 있는 내장함수 randomFloatSpread 함수
-    positions[i * 3] = THREE.MathUtils.randFloatSpread(10);
-    positions[i * 3 + 1] = THREE.MathUtils.randFloatSpread(10);
-    positions[i * 3 + 2] = THREE.MathUtils.randFloatSpread(10);
 
-    colors[i * 3] = Math.random();
-    colors[i * 3 + 1] = Math.random();
-    colors[i * 3 + 2] = Math.random();
-  }
-
-  // 3 부분은 3개의 정보가 모여서 점하나의 정보를 담고있다고 알려주는 것
-  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-  geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-
-  const material = new THREE.PointsMaterial({
-    color: 0xccaaff,
-    size: 0.1,
-    vertexColors: true,
-    // sizeAttenuation: false,
-  });
-
-  const textureLoader = new THREE.TextureLoader();
-
-  const texture = textureLoader.load('./assets/textures/particle.png');
-
-  //넣어준 particle의 이미지가 배경까지 부자연스럽게 보여줘 해결해주는 속성값들
-  material.alphaMap = texture;
-  material.transparent = true;
-  material.depthWrite = false;
-
-  const points = new THREE.Points(geometry, material);
-
-  scene.add(points);
 
   render();
 
