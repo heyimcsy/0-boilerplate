b85b511138d123a70647dde09f9641c0f31e8220
sychoi <cinnamon1995@naver.com>
2024-12-16 12:47:20 +0900

    3차원 공간 생성

--- 6-skybox/src/main.js
+++ 6-skybox/src/main.js
@@ -16,26 +16,41 @@ function init() {
 
   const scene = new THREE.Scene();
 
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500);
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 
   camera.position.z = 5;
 
-  new OrbitControls(camera, renderer.domElement);
+  const controls = new OrbitControls(camera, renderer.domElement);
 
-  const geometry = new THREE.BoxGeometry(3, 3,3);
-  const material = new THREE.MeshPhongMaterial({
-    color: 0xaaccee,
-    side:  THREE.BackSide,
-  });
+  controls.minDistance = 5;
+  controls.maxDistance = 100;
 
-  const skybox = new THREE.Mesh(geometry, material);
+  const textureLoader = new THREE.TextureLoader().setPath('assets/textures/Yokohama/');
+
+  //positive, negative 양의 방향 음의 방향 x축에 대한 양의방향 음의 방향 을 뜻함
+  const images = [
+    'posx.jpg', 'negx.jpg',
+    'posy.jpg', 'negy.jpg',
+    'posz.jpg', 'negz.jpg',
+  ];
+
+  const geometry = new THREE.BoxGeometry(5000, 5000,5000);
+  // const material = new THREE.MeshPhongMaterial({
+  //   color: 0xaaccee,
+  //   side:  THREE.BackSide,
+  // });
+
+  //MeshPhong으로 하면 빛의 영향을 받아 모서리 부분이 음영이 진다. 그래서 빛의 영향을 받지 않는 MeshBasic으로 변경
+  const materials = images.map(image => new THREE.MeshBasicMaterial({
+    map: textureLoader.load(image),
+    side: THREE.BackSide,
+  }));
+
+  //material 을 배열로 보내줄 수 있다. [material1, material2 ,,,,]
+  const skybox = new THREE.Mesh(geometry, materials);
   scene.add(skybox);
 
   render();
-
-  const pointLight = new THREE.PointLight(0xffffff, 2);
-
-  scene.add(pointLight);
 
   function render() {
     renderer.render(scene, camera);
