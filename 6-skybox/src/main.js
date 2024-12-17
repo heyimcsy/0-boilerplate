cb18fc6718b7bc90bfad8013d3cc0be25e1b74a8
sychoi <cinnamon1995@naver.com>
2024-12-17 21:46:21 +0900

    3차원 공간 생성 2번째 방법

--- 6-skybox/src/main.js
+++ 6-skybox/src/main.js
@@ -20,35 +20,53 @@ function init() {
 
   camera.position.z = 5;
 
-  const controls = new OrbitControls(camera, renderer.domElement);
+  /** 큐브맵 텍스처를 이용한 3차원 공간 표현 */
+  // const controls = new OrbitControls(camera, renderer.domElement);
+  //
+  // controls.minDistance = 5;
+  // controls.maxDistance = 100;
+  //
+  // const textureLoader = new THREE.TextureLoader().setPath('assets/textures/Yokohama/');
+  //
+  // //positive, negative 양의 방향 음의 방향 x축에 대한 양의방향 음의 방향 을 뜻함
+  // const images = [
+  //   'posx.jpg', 'negx.jpg',
+  //   'posy.jpg', 'negy.jpg',
+  //   'posz.jpg', 'negz.jpg',
+  // ];
+  //
+  //
+  // const geometry = new THREE.BoxGeometry(5000, 5000,5000);
+  // // const material = new THREE.MeshPhongMaterial({
+  // //   color: 0xaaccee,
+  // //   side:  THREE.BackSide,
+  // // });
+  //
+  // //MeshPhong으로 하면 빛의 영향을 받아 모서리 부분이 음영이 진다. 그래서 빛의 영향을 받지 않는 MeshBasic으로 변경
+  // const materials = images.map(image => new THREE.MeshBasicMaterial({
+  //   map: textureLoader.load(image),
+  //   side: THREE.BackSide,
+  // }));
+  //
+  // //material 을 배열로 보내줄 수 있다. [material1, material2 ,,,,]
+  // const skybox = new THREE.Mesh(geometry, materials);
+  // scene.add(skybox);
 
-  controls.minDistance = 5;
-  controls.maxDistance = 100;
+  /** 큐브맵 텍스처를 이용한 3차원 공간 표현2 */
 
-  const textureLoader = new THREE.TextureLoader().setPath('assets/textures/Yokohama/');
+  const cubeTextureLoader = new THREE.CubeTextureLoader().setPath('assets/textures/Yokohama/');
 
-  //positive, negative 양의 방향 음의 방향 x축에 대한 양의방향 음의 방향 을 뜻함
   const images = [
     'posx.jpg', 'negx.jpg',
     'posy.jpg', 'negy.jpg',
     'posz.jpg', 'negz.jpg',
   ];
 
-  const geometry = new THREE.BoxGeometry(5000, 5000,5000);
-  // const material = new THREE.MeshPhongMaterial({
-  //   color: 0xaaccee,
-  //   side:  THREE.BackSide,
-  // });
+  const cubeTexture = cubeTextureLoader.load(images);
 
-  //MeshPhong으로 하면 빛의 영향을 받아 모서리 부분이 음영이 진다. 그래서 빛의 영향을 받지 않는 MeshBasic으로 변경
-  const materials = images.map(image => new THREE.MeshBasicMaterial({
-    map: textureLoader.load(image),
-    side: THREE.BackSide,
-  }));
+  scene.background = cubeTexture;
 
-  //material 을 배열로 보내줄 수 있다. [material1, material2 ,,,,]
-  const skybox = new THREE.Mesh(geometry, materials);
-  scene.add(skybox);
+  new OrbitControls(camera, renderer.domElement);
 
   render();
 
